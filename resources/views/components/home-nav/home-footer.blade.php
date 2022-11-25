2b1e960190dbbb79adf54f8511226410d60263ca
milka77 <55999046+milka77@users.noreply.github.com>
2022-11-25 19:48:05 +0000

    Added site and social links to footer and changed bg color to red matching to the site design

--- resources/views/components/home-nav/home-footer.blade.php
+++ resources/views/components/home-nav/home-footer.blade.php
@@ -1,18 +1,28 @@
-<footer class="border-t border-zinc-600  bg-violet-900 text-white py-2 mt-auto">
+<footer class="border-t border-zinc-600  bg-red-900 text-white py-2 mt-auto">
   <div class="flex justify-around items-center ">
     <ul class="text-center">
       <li>Quick Links</li>
-      <li>Home</li>
-      <li>Roster</li>
-      <li>Boss tactics</li>
-      <li><a class="hover:text-slate-400" href="https://raider.io/guilds/eu/ragnaros/Sp%C3%A1rtai%20Kem%C3%A1lok" target="_blank">Raider.IO</a></li>
+      <li><a class="hover:text-slate-400" href="{{ route('home') }}">Home</a></li>
+      <li><a class="hover:text-slate-400" href="{{ route('guild.roster') }}" >Roster</a></li>
+      <li><a class="hover:text-slate-400" href="{{ route('raidtax.index') }}">Boss tactics</a></li>
+      <li><a class="hover:text-slate-400" href="{{ route('guild.kisokos') }}">Kemál Kisokos</a></li>
+      <li><a class="hover:text-slate-400" href="{{ route('guild.recruitment') }}">Tagfelvétel</a></li>
     </ul>
-    <ul>
-      <li>F.A.Q.s</li>
+    <ul class="text-center">
+      <li>Social Links</li>
+      <li><a class="hover:text-slate-400" href="https://raider.io/guilds/eu/ragnaros/Sp%C3%A1rtai%20Kem%C3%A1lok" target="_blank">Raider.IO</a></li>
+      <li><a class="hover:text-slate-400" href="https://worldofwarcraft.com/en-gb/guild/eu/ragnaros/sp%C3%A1rtai-kem%C3%A1lok" target="_blank">WoW Armory</a></li>
+      <li><a class="hover:text-slate-400" href="https://www.wowprogress.com/guild/eu/ragnaros/Sp%C3%A1rtai+Kem%C3%A1lok" target="_blank">Wowprogress</a></li>
     </ul>
     <ul class="text-center">
       <li>Member Links</li>
-      <li>Login</li>
+      <li>
+      @if (auth()->user())
+        <a class="hover:text-slate-400" href="{{ route('logout') }}">Logout</a> 
+      @else
+        <a class="hover:text-slate-400" href="{{ route('user.login') }}">Login</a>
+      @endif
+      </li>
       <li>Raids</li>
       
     </ul>
