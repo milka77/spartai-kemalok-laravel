eb6e3afb39010a08ac76e43ea82bda143af232d1
milka77 <55999046+milka77@users.noreply.github.com>
2022-11-28 20:28:48 +0000

    Added responsive features to roster view

--- resources/views/guild/rosterio.blade.php
+++ resources/views/guild/rosterio.blade.php
@@ -1,7 +1,7 @@
 <x-home-master>
   @section('content')
   <div class="flex justify-center">
-    <div class=" w-4/6 p-3 rounded-lg bg-zinc-800 text-white mb-4">
+    <div class="w-full lg:w-4/6 p-3 rounded-lg bg-zinc-800 text-white mb-4">
       <div class="mb-4">
         <h1 class="text-2xl text-center"><a class="text-red-500" href="{{ $members['guild']['url'] }}" target="_blank" rel="noopener noreferrer">{{'<'.$members['guild']['name'].'>'}}</a></h1>
         <p class="text-center">Taglista</p>
@@ -11,18 +11,18 @@
         {{-- Guild Roster Table --}}
         <div class="flex flex-col">
           <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-              <div class="overflow-hidden px-2">
-                <table class="min-w-full">
+            <div class="py-2 inline-block min-w-full sm:px-2 lg:px-8">
+              <div class="overflow-hidden px-0 md:px-2">
+                <table class="min-w-full table-auto">
                   <thead class="border-b border-zinc-500">
                     <tr>
                       <th scope="col" class="text-sm font-medium py-4 text-left">Nev</th>
                       <th scope="col" class="text-sm font-medium py-4 text-left">Role</th>
                       <th scope="col" class="text-sm font-medium py-4 text-left">Rang</th>
-                      <th scope="col" class="text-sm font-medium py-4 text-left">Faj</th>
-                      <th scope="col" class="text-sm font-medium py-4 text-left">Class</th>
-                      <th scope="col" class="text-sm font-medium py-4 text-left">Spec</th>
-                      <th scope="col" class="text-sm font-medium py-4 text-left">Raider IO</th>
+                      <th scope="col" class="text-sm font-medium py-4 text-left hidden md:table-cell">Faj</th>
+                      <th scope="col" class="text-sm font-medium py-4 text-left hidden md:table-cell">Class</th>
+                      <th scope="col" class="text-sm font-medium py-4 text-left hidden md:table-cell">Spec</th>
+                      <th scope="col" class="text-sm font-medium py-4 text-left hidden md:table-cell">Raider IO</th>
                       <th scope="col" colspan="3" class="text-sm font-medium py-4 text-center">Legmagasabb heti runok elozo / aktualis</th>
                     </tr>
                   </thead>
@@ -48,11 +48,11 @@
                             {{ $member->rank }}
                           @endif
                         </td>
-                        <td>{{ $member->character->race }}</td>
-                        <td>{{ $member->character->class }}</td>
-                        <td>{{ $member->character->active_spec_name }}</td>
+                        <td class="hidden md:table-cell">{{ $member->character->race }}</td>
+                        <td class="hidden md:table-cell">{{ $member->character->class }}</td>
+                        <td class="hidden md:table-cell">{{ $member->character->active_spec_name }}</td>
 
-                        <td><a class="text-white hover:text-violet-400" href="{{ $member->character->profile_url }}" target="_blank" rel="noopener noreferrer">Raider IO profil</a></td>
+                        <td><a class="text-white hidden md:table-cell hover:text-violet-400" href="{{ $member->character->profile_url }}" target="_blank" rel="noopener noreferrer">Raider IO profil</a></td>
                         <td class="text-center">
                           @foreach ($whd as $personal_previous_highest_key)
                             @if($member->character->name === $personal_previous_highest_key->name)
