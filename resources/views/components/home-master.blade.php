133d927b8cc30ad8f14e351b76987072372eea7a
milka77 <55999046+milka77@users.noreply.github.com>
2022-12-11 15:31:13 +0000

    Added favicon to the site

--- resources/views/components/home-master.blade.php
+++ resources/views/components/home-master.blade.php
@@ -4,8 +4,16 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
 
+        <title>{{ config('app.name', 'Laravel') }}</title>
+        
+        {{-- Favicon links --}}
+        <link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+        {{-- End Of Favicon links --}}
+        
         <!-- Styles -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
