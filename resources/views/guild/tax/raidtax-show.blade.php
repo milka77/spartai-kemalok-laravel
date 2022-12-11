96debf7ca8cad2a307591ff10e7be9014af30917
milka77 <55999046+milka77@users.noreply.github.com>
2022-12-11 16:14:51 +0000

    Changed display logic to check phase title or body text to exists

--- resources/views/guild/tax/raidtax-show.blade.php
+++ resources/views/guild/tax/raidtax-show.blade.php
@@ -3,16 +3,16 @@
   <div class="flex justify-center">
     <div class=" w-4/6 p-3 rounded-lg bg-zinc-800 text-white mb-4">
       <h1 class="text-2xl text-center mb-4">Raid Taktikák</h1>
-      <h3 class="text-lg text-center mb-4">{{ $tax->raidTaxCategory->name }} / {{ $tax->boss_name }}</h3>
+      <h3 class="text-lg text-center mb-4">{{ $tax->raidTaxCategory->name }} / {{ Str::title($tax->boss_name) }}</h3>
 
-      <div class="border-t border-zinc-600 my-3">
+      <div class="border-t border-b border-zinc-600 my-3">
         <article class="mb-4">
           <span class="p-2 flex mb-3">
             <a class="hover:text-zinc-400" href="{{ route('raidtax.cat.show', $tax->raidTaxCategory->id) }}"><i class="fas fa-arrow-left"></i> Vissza</a>
           </span>
           <h1 class="text-2xl text-center mb-4">{{ Str::title($tax->boss_name) }}</h1>
 
-          <div class="phase-wrapper border-t border-zinc-600 mb-4">
+          <div class="phase-wrapper mb-4">
             <p class="text-lg text-center my-4">{{ $tax->title_1 }}</p>
             <p class="py-2">{!! $tax->body_1 !!}</p>
             @if (!empty($tax->file_path_1))
@@ -26,8 +26,8 @@
             @endif
           </div>
 
-          @if($tax->title_2)
-          <div class="phase-wrapper border-t border-zinc-600 mb-4">
+          @if($tax->title_2 || $tax->body_2)
+          <div class="phase-wrapper mb-4">
             <p class="text-lg text-center my-4">{{ $tax->title_2 }}</p>
             <p class="py-2">{!! $tax->body_2 !!}</p>
             @if (!empty($tax->file_path_2))
@@ -42,8 +42,8 @@
           </div>
           @endif
 
-          @if($tax->title_3)
-          <div class="phase-wrapper border-t border-zinc-600 mb-4">
+          @if($tax->title_3 || $tax->body_3)
+          <div class="phase-wrapper mb-4">
             <p class="text-lg text-center my-4">{{ $tax->title_3 }}</p>
             <p class="py-2">{!! $tax->body_3 !!}</p>
             @if (!empty($tax->file_path_3))
@@ -58,8 +58,8 @@
           </div>
           @endif
 
-          @if($tax->title_4)
-          <div class="phase-wrapper border-t border-zinc-600 mb-4">
+          @if($tax->title_4 || $tax->body_4)
+          <div class="phase-wrapper mb-4">
             <p class="text-lg text-center my-4">{{ $tax->title_4 }}</p>
             <p class="py-2">{!! $tax->body_4 !!}</p>
             @if (!empty($tax->file_path_4))
@@ -74,8 +74,8 @@
           </div>
           @endif
 
-          @if($tax->title_5)
-          <div class="phase-wrapper border-t border-b border-zinc-600 mb-4 pb-4">
+          @if($tax->title_5 || $tax->body_5)
+          <div class="phase-wrapper mb-4 pb-4">
             <p class="text-lg text-center my-4">{{ $tax->title_5 }}</p>
             <p class="py-2">{!! $tax->body_5 !!}</p>
             @if (!empty($tax->file_path_5))
