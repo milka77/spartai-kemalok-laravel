8d1a7c9d75b14c5b8313d94cf475194d3f4cf6ee
Krisztian Keseru <evo3gt@gmail.com>
2022-12-21 11:58:43 +0000

    Refactored raiderio view adjusted style to mobile view and added season M+ score

--- resources/views/guild/raiderio.blade.php
+++ resources/views/guild/raiderio.blade.php
@@ -1,40 +1,40 @@
 <x-home-master>
   @section('content')
-  <div class="flex justify-center">
-    <div class="w-1/2 min-h-full p-3 rounded-lg bg-zinc-800 text-gray-200 mb-5">
+  <div class="flex justify-center px-1">
+    <div class="w-full md:w-2/3 min-h-full p-3 rounded-lg bg-zinc-800 text-gray-200 mb-5">
       <h1 class="text-2xl text-center mb-4">Heti legnagyobb kulcs runok</h1>
 
-      <div class="border-t border-b border-zinc-600 p-2 flex gap-5">
+      <div class="border-t border-b border-zinc-600 mb-3 pt-2 flex flex-col md:flex-row  md:gap-5 md:p-2">
         {{-- Thumbnail pic --}}
-        <div class="my-auto">
+        <div class="mx-auto md:my-auto pb-2 md:pb-0">
           <img class="h-auto w-auto" src="{{ $data->thumbnail_url }}" alt="">
         </div>
         {{-- End Of Thumbnail pic --}}
 
         {{-- Character Info --}}
-        <div>
+        <div class="md:w-1/3">
           <ul>
-            <li>Character: <span class="{{ strtolower($data->class) }}"> {{ $data->name }} </span></li>
-            <li>Race: <span class="{{ strtolower($data->class) }}"> {{ $data->race }} </span></li>
-            <li>Class:  <span class="{{ strtolower($data->class) }}"> {{ $data->class }} </span></li>
-            <li>Achievement Points:  <span class="{{ strtolower($data->class) }}"> {{ $data->achievement_points }} </span></li>
+            <li class="flex justify-between px-2 md:px-0">Character: <span class="{{ strtolower($data->class) }}"> {{ $data->name }} </span></li>
+            <li class="flex justify-between bg-slate-700 md:bg-zinc-800 px-2 md:px-0">Race: <span class="{{ strtolower($data->class) }}"> {{ $data->race }} </span></li>
+            <li class="flex justify-between px-2 md:px-0">Class:  <span class="{{ strtolower($data->class) }}"> {{ $data->class }} </span></li>
+            <li class="flex justify-between bg-slate-700 md:bg-zinc-800 px-2 md:px-0">Achievement Points:  <span class="{{ strtolower($data->class) }}"> {{ $data->achievement_points }} </span></li>
           </ul>
         </div>
         {{-- End Of Character Info --}}
 
         {{-- Gear / Tanlent info --}}
-        <div>
+        <div class="md:w-1/3">
           <ul>
-            <li>ilvl: <span class="{{ strtolower($data->class) }}">{{ $data->gear->item_level_equipped }} </span> </li>
-            <li>
+            <li class="flex justify-between px-2 md:px-0">ilvl: <span class="text-right {{ strtolower($data->class) }}">{{ $data->gear->item_level_equipped }} </span> </li>
+            <li class="flex justify-between bg-slate-700 md:bg-zinc-800 px-2 md:px-0">
               Talent string: <input type=text hidden id="talent_string" value="{{ $data->talentLoadout->loadout_text }}">
               <span class="{{ strtolower($data->class) }}">
                 <a href="https://www.wowhead.com/talent-calc/blizzard/{{ $data->talentLoadout->loadout_text }}" target="_blank" rel="noopener noreferrer"> Talent Calculator  </a>
                 <i class="fas fa-clipboard hover:text-slate-400 cursor-pointer" onclick="copyTalent()"></i>
               </span>
             </li>
-            <li>Active Spec: <span class="{{ strtolower($data->class) }}"> {{ $data->active_spec_name }}  </span></li>
-            <li>Season M+ Score: <span class="{{ strtolower($data->class) }}"> comming soon </span></li>
+            <li class="flex justify-between px-2 md:px-0">Active Spec: <span class="{{ strtolower($data->class) }}"> {{ $data->active_spec_name }}  </span></li>
+            <li class="flex justify-between bg-slate-700 md:bg-zinc-800 px-2 md:px-0">Season M+ Score: <span class="{{ strtolower($data->class) }}"> {{ round($data->mythic_plus_scores->all) }} </span></li>
           </ul>
         </div>
         {{-- End Of Gear / Tanlent info --}}
